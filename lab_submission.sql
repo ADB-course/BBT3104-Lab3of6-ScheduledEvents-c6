c6ff2ede825ad3318e899536bf17d5ba17e800a6
GraceChelangat <grace.chelangat@strathmore.edu>
2024-09-30 22:17:02 +0300

    Scheduled event

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,9 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+  INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)
+  SELECT AVG(waiting_time_minutes) 
+  FROM customer_service_ticket
+  WHERE ticket_timestamp >= NOW() - INTERVAL 1 HOUR;
+SHOW EVENTS;
